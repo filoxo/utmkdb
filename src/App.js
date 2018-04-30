14ffdb3eab9b038f2231897bc737b3edfa81ba8f
Carlos Filoteo <carlosfiloteo@canopytax.com>
2018-04-30 08:56:06 -0600

    Add routes and menu nav

--- src/App.js
+++ src/App.js
@@ -1,75 +1,39 @@
 import React, { Component } from 'react'
 import './App.css'
-import api from './api'
-import Card from './Card'
-import Loading from './Loading'
-import Observer from '@researchgate/react-intersection-observer'
+import List from './List'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
 class App extends Component {
-  state = {
-    records: [],
-    fetchNextPage: null,
-    done: false
-  }
-
-  loadMore = () => {
-    if (!this.state.done && !!this.state.fetchNextPage) {
-      this.setState({ loading: true }, this.state.fetchNextPage())
-    }
-  }
-
-  componentWillMount() {
-    api('Keyboards')
-      .select({
-        view: 'Grid view',
-        fields: ['Name', 'Switches', 'Keycaps', 'Image'],
-        pageSize: 12
-      })
-      .eachPage(
-        (records, fetchNextPage) => {
-          this.setState({
-            records: this.state.records.concat(records),
-            fetchNextPage,
-            loading: false
-          })
-        },
-        err => {
-          if (err) {
-            console.error(err)
-          }
-          this.setState({ done: true, loading: false })
-        }
-      )
-  }
-
   render() {
-    const { records, loading, done } = this.state
     return (
-      <div className="App">
-        <h1 style={{ margin: '1.5rem', textAlign: 'center' }}>UTMK DB</h1>
-        <div
-          style={{
-            display: 'flex',
-            flexFlow: 'wrap',
-            justifyContent: 'center'
-          }}
-        >
-          {records.map(({ fields, id }) => <Card key={id} {...fields} />)}
+      <Router>
+        <div className="App">
+          <nav>
+            <h1
+              style={{
+                display: 'inline-block',
+                margin: '1.5rem',
+                textAlign: 'center'
+              }}
+            >
+              UTMK DB
+            </h1>
+            <Link to="/">List</Link>
+            <Link to="/add">Add</Link>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={List} />
+            <Route
+              path="/add"
+              render={props => <div>{JSON.stringify(props)}</div>}
+            />
+            <Route
+              path="/:id"
+              render={() => <div>Single keybaord record</div>}
+            />
+          </Switch>
         </div>
-        <Observer onChange={this.loadMore} threshold={1}>
-          <div style={{ textAlign: 'center', padding: '1rem 0' }}>
-            {done ? (
-              <span>End of list</span>
-            ) : loading ? (
-              <Loading />
-            ) : (
-              <button type="button" className="btn" onClick={this.loadMore}>
-                Load more
-              </button>
-            )}
-          </div>
-        </Observer>
-      </div>
+      </Router>
     )
   }
 }
