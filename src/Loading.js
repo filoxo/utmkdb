15b21c3e6ad46de55b852d37f37d27edea345817
Carlos Filoteo <carlosfiloteo@canopytax.com>
2018-04-30 02:00:27 -0600

    Add Loading

---
+++ src/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Loading.css'
+
+const Loading = () => (
+  <div class="loading">
+    <div />
+    <div />
+    <div />
+    <div />
+  </div>
+)
+
+export default Loading
