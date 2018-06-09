6796a8e8b4543609032b9632bc92dc6290440fd6
Carlos Filoteo <c.filoteo@live.com>
2018-06-09 16:18:42 -0600

    Add option in UserSelect to add new user

--- src/UserSelect.js
+++ src/UserSelect.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Downshift from 'downshift'
 import api from './api'
+import AddNewUser from './AddNewUser'
 
 export default class UserSelect extends React.Component {
   state = {
     users: [],
-    selectedUser: null
+    selectedUser: null,
+    addNewUser: false
   }
 
   componentDidMount() {
@@ -36,65 +38,76 @@ export default class UserSelect extends React.Component {
 
   render() {
     return (
-      <Downshift
-        onChange={this.props.onSelect}
-        selectedItem={this.props.selectedItem}
-        itemToString={user => (user ? user.name || user.id : '')}
-      >
-        {({ getInputProps, getItemProps, isOpen, highlightedIndex }) => (
-          <div style={{ position: 'relative' }}>
-            <input
-              type="text"
-              required
-              {...getInputProps({
-                placeholder: 'Discord username',
-                className: 'input'
-              })}
-              aria-haspopup="listbox"
-              aria-expanded={isOpen}
-            />
-            <ul
-              style={{
-                border: '1px solid #9195a0',
-                borderTop: 'none',
-                borderBottomLeftRadius: '5px',
-                borderBottomRightRadius: '5px',
-                listStyleType: 'none',
-                margin: 0,
-                maxHeight: 200,
-                overflowY: 'scroll',
-                padding: 0,
-                position: 'absolute',
-                top: '100%',
-                left: '0',
-                right: '0',
-                backgroundColor: '#fff',
-                display: isOpen ? 'block' : 'none'
-              }}
-            >
-              {this.state.users.map((user, index) => (
+      <React.Fragment>
+        <Downshift
+          onChange={this.props.onSelect}
+          selectedItem={this.props.selectedItem}
+          itemToString={user => (user ? user.name || user.id : '')}
+        >
+          {({ getInputProps, getItemProps, isOpen, highlightedIndex }) => (
+            <div style={{ position: 'relative' }}>
+              <input
+                type="text"
+                required
+                {...getInputProps({
+                  placeholder: 'Discord username',
+                  className: 'input'
+                })}
+                aria-haspopup="listbox"
+                aria-expanded={isOpen}
+              />
+              <ul
+                style={{
+                  border: '1px solid #9195a0',
+                  borderTop: 'none',
+                  borderBottomLeftRadius: '5px',
+                  borderBottomRightRadius: '5px',
+                  listStyleType: 'none',
+                  margin: 0,
+                  maxHeight: 200,
+                  overflowY: 'scroll',
+                  padding: 0,
+                  position: 'absolute',
+                  top: '100%',
+                  left: '0',
+                  right: '0',
+                  backgroundColor: '#fff',
+                  display: isOpen ? 'block' : 'none'
+                }}
+              >
                 <li
-                  key={user.id}
-                  {...getItemProps({
-                    item: user,
-                    style: {
-                      backgroundColor:
-                        highlightedIndex === index ? 'rgba(0,0,0,0.1)' : '#fff',
-                      fontWeight:
-                        this.props.selectedItem === user ? 'bold' : 'normal',
-                      padding: '5px'
-                    },
-                    'aria-selected': this.props.selectedItem === user
-                  })}
-                  role="option"
+                  style={{ padding: 5 }}
+                  onClick={() => this.setState({ addNewUser: true })}
                 >
-                  {user.name}
+                  Add new user...
                 </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </Downshift>
+                {this.state.users.map((user, index) => (
+                  <li
+                    key={user.id}
+                    {...getItemProps({
+                      item: user,
+                      style: {
+                        backgroundColor:
+                          highlightedIndex === index
+                            ? 'rgba(0,0,0,0.1)'
+                            : '#fff',
+                        fontWeight:
+                          this.props.selectedItem === user ? 'bold' : 'normal',
+                        padding: 5
+                      },
+                      'aria-selected': this.props.selectedItem === user
+                    })}
+                    role="option"
+                  >
+                    {user.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </Downshift>
+        {this.state.addNewUser && <AddNewUser users={this.state.users} />}
+      </React.Fragment>
     )
   }
 }
